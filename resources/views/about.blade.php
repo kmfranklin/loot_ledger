c2ed63b69ead9d94e25c8dea16c8c0d0483c34a4
Kevin Franklin <kevin@franklindesign.io>
2025-04-30 21:13:22 -0400

    feat: Add public homepage and about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+<div class="max-w-3xl mx-auto py-20 px-4">
+  <h2 class="text-3xl font-semibold mb-4">About Loot Ledger</h2>
+  <p>This application allows Dungeon Masters to browse, customize, and organize loot tables for D&D 5e.
+  You can explore SRD magic items, weapons and armor, and create your own custom versions.</p>
+</div>
+</x-app-layout>
